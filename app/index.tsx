6401a1bfb985753fa714b32039813ff90f3ba8b9
lolloscicchi <lorenzo.scicchitano@edu.itspiemonte.it>
2024-10-01 17:09:09 +0200

    Eliminazione della card nella UI

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,6 @@
 //   Callback: tutte le funzioni che useremo nel progetto
 //   UI: tutto lo stile della schermata
 
-import { Card, cards } from '@/api/data.mock';
 import {
   Text,
   View,
@@ -15,8 +14,6 @@ import {
   ListRenderItem,
   ImageSourcePropType,
 } from 'react-native';
-
-import { CardComponent } from '@/components/molecules/cardComponent/cardComponent.molecule';
 
 import { ButtonComponent } from '@/components/atoms/button/button.atom';
 import { useState } from 'react';
@@ -38,60 +35,7 @@ export default function Index() {
 
   // ** UI ** //
 
-  const renderItem: ListRenderItem<Card> = ({ item, index }) => {
-    return (
-      <View>
-        <CardComponent
-          key={index}
-          title={item.title}
-          subTitle={item.subTitle}
-          backgroundColor={item.backgroundColor}
-          image={item.image}
-        />
-        <ButtonComponent
-          title={'Premi qui'}
-          disabled={false}
-          onPress={function (): void {}} //funzione vuota
-          titleStyle={{ fontSize: 22, fontWeight: 'bold' }}
-        />
-      </View>
-    );
-  };
-  const ItemSeparatorComponent = () => <View style={{ height: 16 }} />;
-  const ListHeaderComponent = () => {
-    return (
-      <Text style={{ fontSize: 24, paddingVertical: 16, textAlign: 'center' }}>
-        Le card di oggi:{' '}
-      </Text>
-    );
-  };
-  const ListFooterComponent = () => {
-    return (
-      <Text style={{ fontSize: 24, paddingVertical: 16, textAlign: 'center' }}>
-        Fine della lista
-      </Text>
-    );
-  };
-
-  const ListEmptyComponent = () => {
-    return (
-      <Text style={{ fontSize: 24, paddingVertical: 16, textAlign: 'center' }}>
-        Nessuna card da mostrare
-      </Text>
-    );
-  };
-
   return (
-    // <FlatList
-    //   style={{ flex: 1 }}
-    //   data={cards}
-    //   renderItem={renderItem}
-    //   bounces={false}
-    //   ItemSeparatorComponent={ItemSeparatorComponent}
-    //   ListHeaderComponent={ListHeaderComponent}
-    //   ListFooterComponent={ListFooterComponent}
-    //   ListEmptyComponent={ListEmptyComponent}
-    // />
     <View style={{ flex: 1, justifyContent: 'center' }}>
       <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}>
         {`Count: ${counter}`}
