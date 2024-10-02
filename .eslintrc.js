d63660ca648ced51ad1b23b4390f2bc9d93e98f4
lolloscicchi <lorenzo.scicchitano@edu.itspiemonte.it>
2024-10-02 14:25:14 +0200

    Aggiunta di Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-  extends: ['expo', 'prettier'],
-  plugins: ['prettier'],
+  extends: [
+    'expo',
+    'prettier',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+  ],
+  plugins: ['prettier', '@react-native'],
   rules: {
     'prettier/prettier': 'error',
   },
