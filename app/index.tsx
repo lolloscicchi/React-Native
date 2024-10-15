b0bce14d052c5da3ee1af4f7d4eb056ef2d5ba42
lolloscicchi <lorenzo.scicchitano@edu.itspiemonte.it>
2024-10-15 17:51:11 +0200

    Fatto es con lo useState (non so se giusto oggi solo bordello)

--- app/index.tsx
+++ app/index.tsx
@@ -7,64 +7,43 @@
 import { Text, View } from 'react-native';
 
 import { ButtonComponent } from '@/components/atoms/button/button.atom';
-import { useCallback, useMemo, useState } from 'react';
+import { TextInputComponent } from '@/components/atoms/textInput/textInput.atom';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { TextInput } from 'react-native-gesture-handler';
 
 const PRICE = 10;
 const DISCOUNT = 2;
 const SPEDITION_COST = 3;
 
 export default function Index() {
-  const [quantity, setQuantity] = useState(0);
+  const [inputText, setInputText] = useState();
+  const [outputText, setOutputText] = useState('');
 
-  const priceState = useMemo(() => {
-    return (PRICE - DISCOUNT) * quantity + SPEDITION_COST;
-  }, [quantity]);
-
-  const increment = useCallback(() => {
-    setQuantity((prevQuantity) => prevQuantity + 1);
+  useEffect(() => {
+    alert('ciao bello');
   }, []);
 
-  const decrement = useCallback(() => {
-    setQuantity((prev) => (prev > 0 ? prev - 1 : 0));
-  }, []);
+  useEffect(() => {
+    setOutputText(inputText);
+  }, [inputText]);
 
   // ** UI ** //
 
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}>
-        Incrementa o decrementa la quantità
-      </Text>
-
-      <View
+      <TextInput
         style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-          marginHorizontal: 'auto',
-        }}>
-        <ButtonComponent
-          title="+ 1"
-          onPress={() => increment()}
-          style={{ marginBottom: 10, backgroundColor: 'lime', padding: 30 }}
-        />
-        <ButtonComponent
-          title="- 1"
-          onPress={() => decrement()}
-          style={{
-            marginBottom: 10,
-            backgroundColor: 'red',
-            padding: 30,
-          }}
-        />
-      </View>
-
-      <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}>
-        Quantità: {`${quantity}`}
-      </Text>
-      <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}>
-        Quantità: {`${priceState}`}
-      </Text>
-      {/* backtic `` */}
+          fontSize: 18,
+          paddingVertical: 10,
+          textAlign: 'center',
+          borderWidth: 2,
+          marginHorizontal: '10%',
+          borderRadius: 10,
+          marginBottom: 10,
+        }}
+        onChangeText={setInputText}
+      />
+      <Text>{`${outputText}`}</Text>
     </View>
   );
 }
