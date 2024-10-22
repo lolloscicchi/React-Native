dc4251d64e121b0ca9282b8bf69a32c016fc2d5d
lolloscicchi <lorenzo.scicchitano@edu.itspiemonte.it>
2024-10-22 15:42:04 +0200

    "Resettato" il progetto per creare una TODO list

--- app/index.tsx
+++ app/index.tsx
@@ -4,42 +4,10 @@
 //   Callback: tutte le funzioni che useremo nel progetto
 //   UI: tutto lo stile della schermata
 
-import { Text, View } from 'react-native';
-
-import { ButtonComponent } from '@/components/atoms/button/button.atom';
-import { TextInputComponent } from '@/components/atoms/textInput/textInput.atom';
-import { useCallback, useEffect, useMemo, useState } from 'react';
-import { TextInput } from 'react-native-gesture-handler';
+import { View } from 'react-native';
 
 export default function Index() {
-  const [inputText, setInputText] = useState('');
-  const [outputText, setOutputText] = useState('');
-
-  useEffect(() => {
-    alert('ciao bello');
-  }, []);
-
-  useEffect(() => {
-    setOutputText(inputText);
-  }, [inputText]);
-
   // ** UI ** //
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <TextInput
-        style={{
-          fontSize: 18,
-          paddingVertical: 10,
-          textAlign: 'center',
-          borderWidth: 2,
-          marginHorizontal: '10%',
-          borderRadius: 10,
-          marginBottom: 10,
-        }}
-        onChangeText={setInputText}
-      />
-      <Text>{`${outputText}`}</Text>
-    </View>
-  );
+  return <View>placeholder</View>;
 }
